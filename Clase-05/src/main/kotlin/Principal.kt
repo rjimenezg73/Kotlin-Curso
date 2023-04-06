a415795286ea0b58b86db24648c0b1637eda7f44
Roberto Jimenez <robertojimenezg@gmail.com>
2023-04-05 18:46:39 -0600

    Código copiado de web oficial

--- Clase-05/src/main/kotlin/Principal.kt
+++ Clase-05/src/main/kotlin/Principal.kt
@@ -1,3 +1,8 @@
-fun main(args: Array<String>){
-    println("Hola Mundo desde Principal")
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+
+fun main() {
+    println("Hello, world!!!")
 }
